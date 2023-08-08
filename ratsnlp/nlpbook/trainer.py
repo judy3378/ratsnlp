9a871012b458088c20922f148fe7df0a332a3d22
Juyeon <81371179+judy3378@users.noreply.github.com>
2023-08-08 12:55:04 +0900

    Update trainer.py

--- ratsnlp/nlpbook/trainer.py
+++ ratsnlp/nlpbook/trainer.py
@@ -3,10 +3,16 @@ import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 
+# Create lists to store values per epoch
+train_loss_per_epoch = []
+val_loss_per_epoch = []
+train_acc_per_epoch = []
+val_acc_per_epoch = []
+
 def get_trainer(args, return_trainer_only=True):
     ckpt_path = os.path.abspath(args.downstream_model_dir)
     os.makedirs(ckpt_path, exist_ok=True)
-
+    
     checkpoint_callback = ModelCheckpoint(
         dirpath=ckpt_path,
         save_top_k=args.save_top_k,
